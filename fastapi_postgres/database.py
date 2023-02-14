d93d0330080d6846240e7a37b1b5240511bd4664
ronitnag04 <ronitnag04@gmail.com>
2023-02-13 23:46:11 -0800

    Errors in fastapi tutorial

--- fastapi_postgres/database.py
+++ fastapi_postgres/database.py
@@ -3,7 +3,7 @@ import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
 # Connecting to database
-DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
+DATABASE_URL = "postgresql://postgres:R12o3n45i6t789$@localhost/test_database"
 engine = _sql.create_engine(DATABASE_URL)
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = _declarative.declarative_base()

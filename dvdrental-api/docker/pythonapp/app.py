96ed39a2faa37e0cc2cdf7f3749669e76fa4a763
ronitnag04 <ronitnag04@gmail.com>
2023-02-22 23:29:18 -0800

    Converted to SQLAlchemy

--- dvdrental-api/docker/pythonapp/app.py
+++ dvdrental-api/docker/pythonapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy_utils import create_view
 import os
@@ -9,29 +10,29 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=False)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+engine = create_engine(os.environ.get('DATABASE_URL'))
+session = Session(engine)
 
-with app.app_context():
-    Base = automap_base()
-    Base.prepare(db.engine, reflect=True)
-    Actor = Base.classes.actor
-    Address = Base.classes.address
-    Category = Base.classes.category
-    City = Base.classes.city
-    Country = Base.classes.country
-    Customer = Base.classes.customer
-    Film = Base.classes.film
-    Film_Actor = Base.classes.film_actor
-    Film_Category = Base.classes.film_category
-    Inventory = Base.classes.inventory
-    Language = Base.classes.language
-    Payment = Base.classes.payment
-    Rental = Base.classes.rental 
-    Staff = Base.classes.staff
-    Store = Base.classes.store
+Base = automap_base()
+Base.prepare(autoload_with=engine, reflect=True)
 
-classMap = {'Actor':Actor, 
+Actor = Base.classes.actor
+Address = Base.classes.address
+Category = Base.classes.category
+City = Base.classes.city
+Country = Base.classes.country
+Customer = Base.classes.customer
+Film = Base.classes.film
+Film_Actor = Base.classes.film_actor
+Film_Category = Base.classes.film_category
+Inventory = Base.classes.inventory
+Language = Base.classes.language
+Payment = Base.classes.payment
+Rental = Base.classes.rental 
+Staff = Base.classes.staff
+Store = Base.classes.store
+
+tableMap = {'Actor':Actor, 
             'Address':Address, 
             'Category':Category, 
             'City':City, 
@@ -47,41 +48,23 @@ classMap = {'Actor':Actor,
             'Staff':Staff,
             'Store':Store}
 
-
-@app.route('/return', methods=['POST'])
-def return_content():
-    body = request.get_json()
-    return body['print']
-
-
-@app.route('/table/<tablename>', methods=['GET'])
+@app.route('/query/table/<tablename>', methods=['GET'])
 def get_table(tablename):
+    if tablename not in tableMap.keys():
+        return f'{tablename} table does not exist'
     table_content = []
-    for item in db.session.query(classMap[tablename]).all():
+    for item in session.query(tableMap[tablename]).all():
         del item.__dict__['_sa_instance_state']
         table_content.append(item.__dict__)
     return jsonify(table_content)
 
-
-@app.route('/payments/customer/<id>', methods=['GET'])
-def get_payments_by_customer(id):
-    payments = []
-    for payment in db.session.query(Payment).filter_by(customer_id=id).all():
-        del payment.__dict__['_sa_instance_state']
-        payments.append(payment.__dict__)
-    return jsonify(payments)
-
-@app.route('/customer', methods=['GET'])
-def get_customer_all():
-    customers = []
-    for customer in db.session.query(Customer).all():
-        del customer.__dict__['_sa_instance_state']
-        customers.append(customer.__dict__)
-    return jsonify(customers)
+@app.route('/query/item', methods=['POST'])
+def generic_query():
+    body = request.get_json()
+    item = session.query(tableMap[body["table"]]).get(body["id"])
+    if item is None:
+        return f'No object found in {tableMap[body["table"]]} with id {body["id"]}'
+    del item.__dict__['_sa_instance_state']
+    return jsonify(item.__dict__)
 
 
-@app.route('/customer/<id>', methods=['GET'])
-def get_customer_by_id(id):
-    customer = db.session.query(Customer).filter_by(customer_id=id).first()
-    del customer.__dict__['_sa_instance_state']
-    return jsonify(customer.__dict__)

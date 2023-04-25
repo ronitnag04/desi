d8e6e8049838ee1bfc736315574f60ebe7319fee
ronitnag04 <ronitnag04@gmail.com>
2023-04-25 01:59:37 -0700

    Updated Image paths

--- database-api/app.py
+++ database-api/app.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+    return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 import api.generic
